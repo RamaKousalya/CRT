528ee7f1a42135c87ebf1dc75f63f3183c85d4f3
RamaKousalya <kghantasala2126@gmail.com>
2025-09-09 11:31:55 +0530

    Add files via upload

---
+++ pattern2.py
@@ -0,0 +1,6 @@
+n = int(input())
+s = 65
+for i in range(n):
+    print(chr(s),chr(90-i),sep='  ')
+    s+=1
+    
---
+++ pattern3.py
@@ -0,0 +1,7 @@
+n = int(input())
+s = 97
+for i in range(n):
+    print(chr(s),chr(s).upper(),sep='  ')
+    #print(chr(s),chr(s-32)
+    s+=1
+    
